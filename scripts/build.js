6fec38ad7c03cdeea1061d6fc7a80f8530ff310a
Miłosz Mandowski <milosz.mandowski@owlsdepartment.com>
2021-11-15 17:09:33 +0100

    refactor: change build process to use rollup and babel

---
+++ scripts/build.js
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const fs = require('fs');
+const ttypescript = require('ttypescript');
+
+const { rollup } = require('rollup');
+const commonjs = require('@rollup/plugin-commonjs');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const tsPlugin = require('@rollup/plugin-typescript');
+const { getBabelOutputPlugin } = require('@rollup/plugin-babel');
+const { default: dts } = require('rollup-plugin-dts');
+
+const packageNames = ['ovee', 'ovee-barba', 'ovee-content-loader'];
+
+async function build() {
+	const packageName = [...process.argv][2];
+	const packagePath = path.resolve(__dirname, `../packages/${packageName}`);
+
+	if (!packageNames.includes(packageName)) {
+		throw Error(`Wrong package name! Passed: ${packageName}`);
+	}
+
+	fs.rmdirSync(path.resolve(packagePath, `dist`), { recursive: true, force: true });
+
+	// eslint-disable-next-line no-unreachable
+	for (const format of ['es', 'cjs']) {
+		const bundle = await rollup({
+			external: ['ovee.js'],
+			input: path.resolve(packagePath, `src/index.ts`),
+			plugins: [
+				commonjs(),
+				nodeResolve(),
+				tsPlugin({
+					typescript: ttypescript,
+					tsconfig: path.resolve(packagePath, `tsconfig.json`),
+				}),
+			],
+		});
+
+		await bundle.generate({});
+		await bundle.write({
+			format,
+
+			plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] })],
+			file: path.resolve(packagePath, 'dist', format === 'es' ? 'index.esm.js' : 'index.js'),
+		});
+		await bundle.close();
+	}
+
+	await generateDeclarationFile(packageName, packagePath);
+}
+
+async function generateDeclarationFile(packageName, packagePath) {
+	const bundle = await rollup({
+		input: path.resolve(packagePath, `dist/types/index.d.ts`),
+		plugins: [dts()],
+	});
+
+	await bundle.generate({});
+	await bundle.write({
+		file: path.resolve(packagePath, `dist/index.d.ts`),
+		format: 'es',
+	});
+	await bundle.close();
+
+	fs.rmdirSync(path.resolve(packagePath, `dist/types`), {
+		recursive: true,
+	});
+}
+
+build();
