ba51840818f853d179d78ce7551dce51dc4c760e
Miłosz Mandowski <milosz.m20@gmail.com>
2023-02-22 15:01:21 +0100

    fix(ovee): update typings for `emitEvent` helper

--- packages/ovee/src/dom/emitEvent.ts
+++ packages/ovee/src/dom/emitEvent.ts
@@ -1,6 +1,10 @@
 import { AppEvent } from './AppEvent';
 
-export function emitEvent(targetElement: Element, eventDesc: Event | string, detail?: any): void {
+export function emitEvent(
+	targetElement: EventTarget,
+	eventDesc: Event | string,
+	detail?: any
+): void {
 	const event = eventDesc instanceof Event ? eventDesc : new AppEvent(eventDesc, { detail });
 
 	targetElement.dispatchEvent(event);
